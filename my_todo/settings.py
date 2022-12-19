2b63cbba130f207a79c32b177880b03f4bc4e825
samezzz <95895530+samezzz@users.noreply.github.com>
2022-12-19 23:28:27 +0000

    preparing for deployment

--- my_todo/settings.py
+++ my_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
